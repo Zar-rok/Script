81d5b41e2d671ab227a129cb0c40ef7eae9f14c7
Zar-rok <zar_rok@live.fr>
2017-01-31 16:24:32 +0100

    Update fillScreen.sh

--- Misc/fillScreen.sh
+++ Misc/fillScreen.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 elements=("░" "▒" "▓")
-width=$(tput cols)
-height=$(tput lines)
+width=$(($(tput cols) + 1))
+height=$(($(tput lines) + 2))
 
+clear
+trap 'printf "\x1B[0m"; clear; exit' 2
 while :
 do
+	tput setaf $(($RANDOM%256))
 	printf "\033[%d;%dH%s" $(($RANDOM%$height)) $(($RANDOM%$width)) ${elements[$(($RANDOM%3))]}
 done
