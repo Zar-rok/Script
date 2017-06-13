d7d6af8b3af84997826a27ea69f7dffb5e5f8e95
Zar-rok <zar_rok@live.fr>
2017-06-13 17:52:34 +0200

    add script to convert tab to space

---
+++ Util/tabToSpace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]
+then
+	printf "[#] Usage : %s [directory] [pattern to search] [nbr space]\n" "$0"
+	exit 2
+fi
+
+printf "[*] Processed files :\n"
+find "$1" -name "$2" ! -type d -exec bash -c "expand -t $3 \"\$0\" > /tmp/tmp_file && mv /tmp/tmp_file \"\$0\" && printf \"    [-] \$0\n\"" {} \;
