2693dce160259922dc2fe725fc9602615f70b884
Zar-rok <zar_rok@live.fr>
2017-08-12 18:21:05 +0200

    lock the session and display a pixelized screenshot

---
+++ Util/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Take a screenshot and pixelize it
+convert x:root -scale 10% -scale 1000% /tmp/screen.png
+
+# Add the given picture in the middle of the screenshot
+if [ -f "$1" ]
+then
+  convert /tmp/screen.png "$1" -gravity center -composite -matte /tmp/screen.png
+fi
+
+# Lock and display the screenshot
+i3lock -u -i /tmp/screen.sh
+
+rm /tmp/screen.png
