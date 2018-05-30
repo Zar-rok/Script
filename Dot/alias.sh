29c82233eca912002329253c25be817d6bc8e875
Zar-rok <zar_rok@live.fr>
2018-05-30 10:02:14 +0200

    ajout d'alias pour l'ip des containers Docker

--- Dot/alias.sh
+++ Dot/alias.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 function find_file() {
-        if [ $# -lt 2 ]
-        then
-                printf "Usage : find_file [Directory] [String to find]"
-        else
-                grep -rnw "$1" -e "$2"
-        fi
+  if [ "$#" -lt 2 ]
+  then
+    printf "[#] Usage : find_file [Directory] [String to find]"
+  else
+    grep -rnw "$1" -e "$2"
+  fi
 }
-
 alias find_file="find_file"
+
+function docker_ip() {
+  if [ "$#" -lt 1 ]
+  then
+    printf "[#] Usage : docker_ip [container name]"
+  else
+    docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$1"
+  fi
+}
+alias docker_ip="docker_ip"
