b7dcf58f8e6c456532e38a2a13387c0974ce2498
Zar-rok <zar_rok@live.fr>
2018-11-21 19:49:38 +0100

    Add more alias

--- Dot/alias.sh
+++ Dot/alias.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# ripgrep is also good
 function find_file() {
   if [ "$#" -lt 2 ]
   then
-    printf "[#] Usage : find_file [Directory] [String to find]"
+    printf "[#] Usage : %s [Directory] [String to find]" "$0"
   else
     grep -rnw "$1" -e "$2"
   fi
@@ -12,8 +13,16 @@ function find_file() {
 function docker_ip() {
   if [ "$#" -lt 1 ]
   then
-    printf "[#] Usage : docker_ip [container name]"
+    printf "[#] Usage : %s [Container name]" $0"
   else
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$1"
   fi
 }
+
+alias open-rc="nano ~/.zshrc"
+alias relo-rc=". ~/.zshrc"
+alias 2up="sudo apt update && sudo apt upgrade"
+alias open="xdg-open"
+
+# for xcas
+alias calc="giac"
