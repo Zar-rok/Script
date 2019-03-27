b3112dca87403ca09814d87188789b723d65861f
Zar-rok <zar_rok@live.fr>
2019-03-27 22:12:41 +0100

    Add tainting

--- Util/lock.sh
+++ Util/lock.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+FILE_NAME="screen.png"
+FILE_PATH="/tmp"
+FILE_FULL="${FILE_PATH}/${FILE_NAME}"
+
 # Take a screenshot and pixelize it
-convert x:root -scale 10% -scale 1000% /tmp/screen.png
+scrot "${FILE_NAME}" -e "mv ${FILE_NAME} ${FILE_PATH}"
+
+TAINT=$(printf "%02x%02x%02x\n" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
+convert "${FILE_FULL}" -scale 10% -scale 1000% -blur 0x1 -fill "#${TAINT}" -tint 50 "${FILE_FULL}"
 
 # Add the given picture in the middle of the screenshot
 if [ -f "$1" ]
 then
-  convert /tmp/screen.png "$1" -gravity center -composite -matte /tmp/screen.png
+  convert "${FILE_FULL}" "$1" -gravity center -composite -matte "${FILE_FULL}"
 fi
 
 # Lock and display the screenshot
-i3lock -u -i /tmp/screen.png
+i3lock -u -i "${FILE_FULL}"
 
-rm /tmp/screen.png
+rm "${FILE_FULL}"
